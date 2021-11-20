46f11ca8be77d46275cdc23faa52a25e97f77a22
seanlivingstone@tamu.edu <>
2021-11-20 10:06:01 -0600

    Changes the bg color to off-white

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 // background.js
 
-let color = '#3aa757';
+let color = '#f8f0e3';
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+    console.log('Changing the background color', `color: ${color}`);
 });
+
